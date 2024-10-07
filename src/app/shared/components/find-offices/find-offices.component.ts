18a7bc2a924feb0a56b10f907fe0bf2d5415ea9c
DarDev <darwin_0.10@hotmail.com>
2024-10-06 22:03:44 -0500

    refactor: :recycle: update offices for qas

--- src/app/shared/components/find-offices/find-offices.component.ts
+++ src/app/shared/components/find-offices/find-offices.component.ts
@@ -97,10 +97,10 @@ export class FindOfficesComponent implements OnInit, OnDestroy {
 
     this.company = structuredClone(CompanyObj);
 
-    // if (!environment.production) {
-    //   this.company.splice(0, 3);
-    //   this.company.push({ name: "PRUEBAS", code: "prb" });
-    // }
+    if (true) {
+      this.company.splice(0, 3);
+      this.company.push({ name: "PRUEBAS", code: "prb" });
+    }
   }
 
   public changeCompany(): void {
